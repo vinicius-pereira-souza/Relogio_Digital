3a66f52f3b1843d4721bd064beaf3ff711fe815d
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2022-10-26 18:12:54 -0300

    projeto pessoal relógio digital

---
+++ script.js
@@ -0,0 +1,45 @@
+const spans = document.querySelectorAll('span')
+const bg = document.querySelector('.bg')
+
+function handleTimeClock() {
+  const dataToday = new Date()
+
+  const time = {
+    hour: dataToday.getHours(), 
+    minute: dataToday.getMinutes(), 
+    segund: dataToday.getSeconds()
+  }
+
+  innerValueZero(spans[0], time.hour)
+  innerValueZero(spans[1], time.minute)
+  innerValueZero(spans[2], time.segund)
+
+  handleBackGround(time.hour)
+}
+
+function innerValueZero(item, dataTimer) {
+  if(dataTimer < 10) {
+    item.innerText = `0${dataTimer}`
+  } else {
+    item.innerText = dataTimer
+  }
+}
+
+function handleBackGround(hour) {
+  if(hour < 13) {
+    bg.classList.remove(bg.classList[1])
+    bg.classList.add('morning')
+  } else if (hour > 13 && hour < 17){
+    bg.classList.remove(bg.classList[1])
+    bg.classList.add('afternoon')
+  } else if (hour > 17){
+    bg.classList.remove(bg.classList[1])
+    bg.classList.add('night')
+  }
+}
+
+console.log(bg.classList[1])
+
+setInterval(() => {
+  handleTimeClock()
+}, 1000);
